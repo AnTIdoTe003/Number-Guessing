6094328e9eb4f1c7fd4075e7c357093455d16b5f
Debmalya Biswas <68019329+AnTIdoTe003@users.noreply.github.com>
2021-06-28 10:58:55 +0530

    Add files via upload

---
+++ numberguess.py
@@ -0,0 +1,38 @@
+import random
+import math
+
+lower = int(input("Enter Lower bound:- "))
+
+upper = int(input("Enter Upper bound:- "))
+
+
+x = random.randint(lower, upper)
+
+print("\nYou've only 5 chances to guess the correct number")
+
+
+count = 0
+
+
+while count < 5:
+	count += 1
+
+	# taking guessing number as input
+	guess = int(input("Guess a number:- "))
+
+	# Condition testing
+	if x == guess:
+		print("Congratulations you did it in ",
+			count, " try")
+		break
+	elif x > guess:
+		print("You guessed too small!")
+	elif x < guess:
+		print("You Guessed too high!")
+
+
+if count >= 5:
+	print("\nThe number is %d" % x)
+	print("\tBetter Luck Next time!")
+
+
